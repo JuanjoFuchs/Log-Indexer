6378dd52661e575eb36df084b6f5ea89110016f2
Juan José Fuchs <juanjo.fuchsc@gmail.com>
2013-08-12 19:06:43 -0500

    fix project structure

--- LogIndexer/Properties/AssemblyInfo.cs
+++ LogIndexer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("91c83ac9-0ee4-4b40-9570-3d65f58395d7")]
+[assembly: Guid("745db4a7-1a7e-4845-aca0-b31ceb5fa7a1")]
 
 // Version information for an assembly consists of the following four values:
 //
