03e3caec8470e1d5c340d2c5e83f7d986b5bb9d7
Juan José Fuchs <juanjo.fuchsc@gmail.com>
2013-08-21 21:29:06 -0500

    added machine name to info

--- LogIndexer.Console/Program.cs
+++ LogIndexer.Console/Program.cs
@@ -1,8 +1,10 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 
 namespace LogIndexer.Console {
   internal class Program {
     static void Main(string[] args) {
+      var hostName = Environment.MachineName;
       var path = ConfigurationManager.AppSettings["Path"];
       var dataDirectory = ConfigurationManager.AppSettings["DataDirectory"];
       var logger = new ConsoleLogger();
@@ -19,7 +21,7 @@ namespace LogIndexer.Console {
       logIndexer.StartWatching();
       logger.WriteLine("Done!");
 
-      logger.WriteLine("Visit http://localhost:8080 to view the indexed data");
+      logger.WriteLine("Visit http://{0}:8080 to view the indexed data", hostName);
 
       System.Console.ReadLine();
       logger.WriteLine("Stopping...");
