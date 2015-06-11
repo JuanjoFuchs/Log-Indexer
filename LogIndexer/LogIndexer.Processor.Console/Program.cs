8a2539ea70d5b89888f8536b2f6e79079ceba62b
Juan José Fuchs <JuanJose.Fuchs@cignium.com>
2015-06-11 15:41:19 -0500

    indexing file

---
+++ LogIndexer/LogIndexer.Processor.Console/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace LogIndexer.Processor.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var path = @"C:\dev\stash\noble-implementation\Cignium.CallCenter\Cignium.CallCenter.Web\CUY.log";
+            var lastLineNumber = 0;
+
+            System.Console.WriteLine("Starting to read '{0}'", path);
+
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var reader = new StreamReader(stream))
+            using (IDocumentStore store = new DocumentStore {Url = "http://localhost:8080", DefaultDatabase = "test"})
+            {
+                store.Initialize();
+
+                using (var bulkInsert = store.BulkInsert())
+                {
+                    try
+                    {
+                        var index = 0;
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            index++;
+                            if (index > lastLineNumber)
+                            {
+                                bulkInsert.Store(new Record {Data = line});
+                                System.Console.Write(".");
+                            }
+                        }
+                    }
+                    catch (EndOfStreamException)
+                    {
+                        System.Console.WriteLine("Reached end of file");
+                    }
+                }
+            }
+
+        }
+    }
+
+    public class Record
+    {
+        public string Data { get; set; }
+    }
+}
